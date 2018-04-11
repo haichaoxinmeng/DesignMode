5ecef8d720241309c75e670fb2073406081fe04a
chenqiang <chenqiang@sanduspace.cn>
2018-04-11 11:17:56 +0800

    测试提交

--- src/main/java/cn/roc/chapters_one/Operation.java
+++ src/main/java/cn/roc/chapters_one/Operation.java
@@ -1,5 +1,6 @@
 package cn.roc.chapters_one;
 
+
 /**
  * @Author Roc Chen
  * @Description 简单工厂模式----运算超类
@@ -7,5 +8,7 @@ package cn.roc.chapters_one;
  * @Modified By:
  */
 public class Operation {
-
+    public static void main(String[] args) {
+        System.out.println("jjjjjjjjjjjjjjjjjj");
+    }
 }
