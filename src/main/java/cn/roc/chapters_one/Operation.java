c1b2f67745c31c9b3cfa48fbe35eb539763900b5
chenqiang <chenqiang@sanduspace.cn>
2018-04-11 11:06:04 +0800

    commit all file

---
+++ src/main/java/cn/roc/chapters_one/Operation.java
@@ -0,0 +1,11 @@
+package cn.roc.chapters_one;
+
+/**
+ * @Author Roc Chen
+ * @Description 简单工厂模式----运算超类
+ * @Date:Created Administrator in 10:47 2018/4/11 0011
+ * @Modified By:
+ */
+public class Operation {
+
+}
