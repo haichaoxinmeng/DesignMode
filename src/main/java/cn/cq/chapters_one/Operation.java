75d2ab8530e1f50221173776856e884a41e51ad2
chenqiang <chenqiang@sanduspace.cn>
2018-04-13 11:31:57 +0800

    测试提交

---
+++ src/main/java/cn/cq/chapters_one/Operation.java
@@ -0,0 +1,14 @@
+package cn.cq.chapters_one;
+
+/**
+ * @Author：chenqiang
+ * @Description：简单工厂模式----运算超类
+ * @Date：2018/4/13 0013 11:30
+ * @Modified By：
+ */
+public class Operation {
+
+    public static void main(String[] args) {
+        System.out.println("jjjjjjjjjjjjjjjjjj");
+    }
+}
